69319b07bec812ca0d35c3902077f41ebf76b210
Furious bEz <furious.bez@fgclive.com>
2017-08-31 12:41:11 -0700

    Uploaded New Service Account

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 // TODO: Enter the path to your service account json file
 // Need help with this step go here: https://firebase.google.com/docs/admin/setup
 
-const serviceAccount = require("./fgsn-madexp-firebase-adminsdk-0k933-04b8e4b9e2.json");
+const serviceAccount = require("./fgsn-madexp-firebase-adminsdk-0k933-db20c4889b.json");
 
 
 // TODO: Enter your database url from firebase
