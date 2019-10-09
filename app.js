f6864b4b4f1dfd028989138ce3274b122153caa0
furious-bez <furious.bez@fgclive.com>
2019-10-09 16:31:57 -0700

    Merge branch 'master' of git://github.com/birdwell/Madden-Companion-Exporter into birdwell-master
    # Conflicts:
    #	app.js
    #	package.json

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const app = express();
 
 // TODO: Enter the path to your service account json file
 // Need help with this step go here: https://firebase.google.com/docs/admin/setup
-const serviceAccount = require("./REPLACE_WITH_SERVICE_ACCOUNT.json");
+const serviceAccount = require("./fgsn-madexp-firebase-adminsdk-0k933-db20c4889b.json");
 
 // TODO: Enter your database url from firebase
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://<DATABASE_NAME>.firebaseio.com/"
+  databaseURL: "https://fgsn-madexp.firebaseio.com/"
 });
 
 // Setup
